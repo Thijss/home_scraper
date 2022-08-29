55688c724fddc5e1ffa3048f2169dc6808b1607a
Thijs Baaijen <Thijss@users.noreply.github.com>
2022-08-29 15:05:45 +0200

    print trigger

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ from home_scraper.core import run
 
 def handler(event: dict, context: dict):
     print("#### START ####")
-    print("EVENT:", event)
-    print("CONTEXT:", context)
+    if trigger := event.get("trigger"):
+        print("trigger", trigger)
     nr_of_new_homes = run()
     print("#### END ####")
     return {"new_homes": nr_of_new_homes}
